adfcc7f8869fe427020c82c69c8d481a20d1edfe
Santiago Pérez <santiagoperezcastro365@gmail.com>
2020-10-17 16:41:22 -0500

    multiply function added tested

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,13 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hola nadie");
+        System.out.println(multiply(5));
+    }
+
+    public static int multiply(int n){
+        if(n < 3){
+            return n;
+        }
+        return multiply(n-1) * multiply(n-2);
     }
 }
